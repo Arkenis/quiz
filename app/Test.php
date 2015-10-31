9b11e2a4417ad08bcc7b6280a15a2c38166dcf9e
Misha <mihail_ponomaryow@gmail.com>
2015-10-31 18:11:12 +0500

    tests index

--- app/Test.php
+++ app/Test.php
@@ -4,5 +4,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-
+    function getName()
+    {
+        $user = User::find($this->user_id);
+        return $user->name;
+    }
 }
